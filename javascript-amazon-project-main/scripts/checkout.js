915a38c2e04643b667e436fde41825e5d0e40df4
lyk336 <134726237+lyk336@users.noreply.github.com>
2023-08-23 15:22:30 +0300

    Fix price formating and radio selectors

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let cartSummaryHTML = '';
 
@@ -21,8 +22,8 @@ cart.forEach((cartItem) => {
         <img class="product-image" src="${matchingProduct.image}" />
 
         <div class="cart-item-details">
-          <div class="product-name"${matchingProduct.name}</div>
-          <div class="product-price">$${matchingProduct.price}</div>
+          <div class="product-name">${matchingProduct.name}</div>
+          <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
           <div class="product-quantity">
             <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
             <span class="update-quantity-link link-primary"> Update </span>
@@ -33,21 +34,21 @@ cart.forEach((cartItem) => {
         <div class="delivery-options">
           <div class="delivery-options-title">Choose a delivery option:</div>
           <div class="delivery-option">
-            <input type="radio" checked class="delivery-option-input" name="delivery-option-1" />
+            <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
             <div>
               <div class="delivery-option-date">Tuesday, June 21</div>
               <div class="delivery-option-price">FREE Shipping</div>
             </div>
           </div>
           <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+            <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
             <div>
               <div class="delivery-option-date">Wednesday, June 15</div>
               <div class="delivery-option-price">$4.99 - Shipping</div>
             </div>
           </div>
           <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-1" />
+            <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}" />
             <div>
               <div class="delivery-option-date">Monday, June 13</div>
               <div class="delivery-option-price">$9.99 - Shipping</div>
